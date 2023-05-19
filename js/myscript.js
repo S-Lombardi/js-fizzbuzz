6f79cb59e2d2a032ed2211047c50b42d3627cfb5
S-Lombardi <silo.lombardi@gmail.com>
2023-05-19 16:44:35 +0200

    Aggiunta quarta condizione

--- js/myscript.js
+++ js/myscript.js
@@ -1,26 +1,26 @@
+"use strict";
 
 // ciclo per stampare in console numeri da 1 a 100
 for(let i=1 ; i<=100 ; i++) {
-    console.log (i);
-    
-
    
     if ( (i % 3 === 0) && (i % 5 === 0)) {
-        console.log( i + " FizzBuzz");
+        console.log( " FizzBuzz");
     }
 
 
     else if( i % 3 === 0) {
-        console.log( i + " Fizz");
+        console.log( " Fizz");
     }
     
    
     else if ( i % 5 ===0) {
-        console.log( i +" Buzz"); 
+        console.log( " Buzz"); 
 
-    }
-
+    } 
     
-        
+    else {
+        console.log (i);
+    }
+   
 
 }
