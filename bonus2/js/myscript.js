328eeb2f53ce854960c6f17dde7ae4f8268c5e24
S-Lombardi <silo.lombardi@gmail.com>
2023-05-20 17:14:18 +0200

    Aggiunte classi di stile alle celle

--- bonus2/js/myscript.js
+++ bonus2/js/myscript.js
@@ -6,33 +6,41 @@ for(let i=1 ; i<=100 ; i++) {
 
     //creo elemento div 
     let cella = document.createElement("div");
+    cella.classList.add("box");
 
     //Se il numero è un multiplo di 3 e 5 stampa FizzBuzz
     if ( (i % 3 === 0) && (i % 5 === 0)) {
         console.log( " FizzBuzz"); 
         cella.innerHTML =" FizzBuzz";
+        cella.classList.add("bg-1");
     }
 
     //Se il numero è un multiplo di 3 stampa Fizz
     else if( i % 3 === 0) {
         console.log( " Fizz");
         cella.innerHTML = " Fizz";
+        cella.classList.add("bg-2");
     }
 
     //Se il numero è un multiplo di 5 stampa Buzz
     else if ( i % 5 ===0) {
         console.log( " Buzz");
         cella.innerHTML =" Buzz";
+        cella.classList.add("bg-3");
     } 
     
     //Altrimenti stampa gli altri numeri
     else {
         console.log (i);
         cella.innerHTML = i ;
+        cella.classList.add("bg-4");
     }
 
     //visualizzo a video nel container i numeri e le stringhe
     document.getElementById("container");
     container.append(cella);
-      
+
+    document.getElementById("row");
+    row.append(cella);
+   
 }
